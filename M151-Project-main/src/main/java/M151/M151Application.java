0cdad8ebe10b46fbf99577d4e2a8b0ef79ffabbe
SimonRuckli <55438072+SimonRuckli@users.noreply.github.com>
2021-06-04 19:42:29 +0200

    fix project structure

---
+++ M151-Project-main/src/main/java/M151/M151Application.java
@@ -0,0 +1,12 @@
+package M151;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M151Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M151Application.class, args);
+	}
+}
---
+++ M151-Project-main/src/main/java/M151/model/User.java
@@ -0,0 +1,66 @@
+package M151.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.ColumnTransformer;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "articleShop_user")
+@NamedQuery(name = "User.checkPassword", query = "SELECT u FROM User u WHERE u.username = :username and password = public.crypt(text(:password), text(password))")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    @SequenceGenerator(allocationSize = 1, name = "user_sequence")
+    @Column(name = "id", nullable = false, updatable = false)
+    private long userID;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    @ColumnTransformer(write = "crypt(?, gen_salt('bf', 8))")
+    private String password;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    protected User() {
+    }
+
+    public User(final String username, final String firstname, final String lastname, final String password, final UserRole userRole) {
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.userRole = userRole;
+    }
+}
